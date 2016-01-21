66fcc53c53e0a54be13c0bd5ba4755485738d038
Christian Gibbs <christiangibbs@christians-imac.northgate.local>
2016-01-21 12:34:14 +0000

    Accordion added

--- js/actions.js
+++ js/actions.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
         $(this).parent().fadeOut();
     });
 
+    // Accordion
+    var allPanels = $('.accordion > dd').hide();
+    $('.accordion > dt').click(function() {
+      if ($(this).next().is(':hidden')) {
+        $(this).next().slideDown();
+        $('.accordion > dd').not($(this).next()).slideUp();
+      }
+      else {
+        $(this).next().slideUp();
+      }
+    });
+
 });
